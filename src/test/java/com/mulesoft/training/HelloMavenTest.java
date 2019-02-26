864fef94768866127513e9e36f8db15bce5a5b8e
levimodesto.ti <levimodesto.ti@gmail.com>
2019-02-26 16:48:20 -0300

    Added Content-Type header to response

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -17,7 +17,6 @@ public class HelloMavenTest extends FunctionalTestCase {
     protected String getConfigFile() {
         return "maven-project-new.xml";
     }
-
     
     @Test
     public void retrieveFlightsAddsAppropriateHeader() throws Exception {
